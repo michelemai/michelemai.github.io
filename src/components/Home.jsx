eada731f77bb525cc575857a5809bd6fb3445fd5
Michele Mai <michele8231@gmail.com>
2025-04-22 16:17:24 -0700

    templates test

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+      <section id="home">
+        <h1>Hello, I’m [Michele]</h1>
+        <p>Welcome to my portfolio website!</p>
+        <p>I'm a passionate web developer...</p>
+      </section>
+    );
+  }
+  
+  export default Home;
